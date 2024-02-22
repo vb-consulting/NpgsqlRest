d31363e5fd410516c763fccd96b79d7edc0e7353
Vedran B <vbilopav@gmail.com>
2024-02-22 11:01:42 +0100

    benchmarks

--- BenchmarkTests/Program.cs
+++ BenchmarkTests/Program.cs
@@ -6,13 +6,16 @@ using BenchmarkDotNet.Toolchains.InProcess.Emit;
 using BenchmarkTests;
 using Perfolizer.Horology;
 
-BenchmarkRunner
-    .Run<HttpClientTests>(
-        DefaultConfig.Instance.AddJob(
-            Job.Default.WithToolchain(new InProcessEmitToolchain(timeout: TimeSpan.FromSeconds(9), logOutput: false))
-            .WithLaunchCount(1)
-            .WithWarmupCount(5)
-            .WithIterationCount(100)
-            .WithIterationTime(TimeInterval.FromMilliseconds(80)))
-        .AddLogger(new ConsoleLogger(unicodeSupport: true, ConsoleLogger.CreateGrayScheme()))
-        .WithOptions(ConfigOptions.DisableLogFile));
+
+BenchmarkRunner.Run<HttpClientTests>();
+
+//BenchmarkRunner
+//    .Run<HttpClientTests>(
+//        DefaultConfig.Instance.AddJob(
+//            Job.Default.WithToolchain(new InProcessEmitToolchain(timeout: TimeSpan.FromSeconds(9), logOutput: false))
+//            .WithLaunchCount(1)
+//            .WithWarmupCount(5)
+//            .WithIterationCount(100)
+//            .WithIterationTime(TimeInterval.FromMilliseconds(80)))
+//        .AddLogger(new ConsoleLogger(unicodeSupport: true, ConsoleLogger.CreateGrayScheme()))
+//        .WithOptions(ConfigOptions.DisableLogFile));
